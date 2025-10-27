cadea13969a3964d87c6c7ce044f31c266a2510c
Rechtsanwalt <marianonavarro2021@hotmail.com>
2025-10-27 12:20:40 +0100

    Create exercise 22-2

---
+++ ejercicios-clase22-2/README.md
@@ -0,0 +1,9 @@
+# Eercicio Clase 2-2
+
+## Se solicita:
+
+Número de enlaces de la página
+Dirección a la que enlaza el penúltimo enlace
+Numero de enlaces que enlazan a http://prueba
+Número de enlaces del tercer párrafo
+AYUDA: Si queremos acceder al contenido de un atributo de un nodo elemento es suficiente con usar nodo.atributo (ej: p.id accede al contenido del id del nodo almacenado en p)
---
+++ ejercicios-clase22-2/script.js
@@ -0,0 +1,26 @@
+// Obtener todos los enlaces de la página
+const enlaces = document.getElementsByTagName("a");
+console.log("Número de enlaces de la página:", enlaces.length);
+
+// Dirección a la que enlaza el penúltimo enlace
+if (enlaces.length >= 2) {
+  const penultimo = enlaces[enlaces.length - 2];
+  console.log("Dirección del penúltimo enlace:", penultimo.href);
+}
+
+// Número de enlaces que enlazan a "http://prueba"
+let contadorPrueba = 0;
+for (let i = 0; i < enlaces.length; i++) {
+  if (enlaces[i].href === "http://prueba") {
+    contadorPrueba++;
+  }
+}
+console.log("Número de enlaces que enlazan a 'http://prueba':", contadorPrueba);
+
+// Número de enlaces del tercer párrafo
+const parrafos = document.getElementsByTagName("p");
+if (parrafos.length >= 3) {
+  const tercerParrafo = parrafos[2];
+  const enlacesTercerParrafo = tercerParrafo.getElementsByTagName("a");
+  console.log("Número de enlaces del tercer párrafo:", enlacesTercerParrafo.length);
+}
